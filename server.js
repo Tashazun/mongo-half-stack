0898b9b4fb72e302338cc33ac17f4d974261c196
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-13 10:22:02 -0700

    server connection works, but fails to find mongodb file

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 /* eslint no-console: off */
-const chaiHttp = require('chai-http');
+const http = require('http');
 
 const app = require('./lib/app');
-const mongobd = require('.lib/mongodb');
+const mongodb = require('.lib/mongodb');
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/bands';
 
+mongodb.connect(MONGODB_URI)
+    .then(() => console.log('Connected to Bands Collection'))
+    .catch(err => console.log('FAILED!!', err));
 
+const server = http.createServer(app);
+const PORT = process.env.MONGODB_URI;
+
+server.listen(PORT, () => {
+    console.log('Listening to PORT');
+});
